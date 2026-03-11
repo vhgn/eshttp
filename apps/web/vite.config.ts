f1999a6eccfc337fecbdf1e552835252c22c4068
Vahagn <metavahagn@gmail.com>
2026-03-11 23:39:56 +0400

    ci: add nitro to host on Vercel

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,6 +3,7 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { nitro } from 'nitro/vite';
 
 const config = defineConfig({
 	plugins: [
@@ -23,6 +24,7 @@ const config = defineConfig({
 			],
 		}),
 		viteReact(),
+		nitro(),
 	],
 });
 
